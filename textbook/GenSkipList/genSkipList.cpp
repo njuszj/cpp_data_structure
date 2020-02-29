99da981465accbd9bc748caedf7ad675f14da77b
njuszj <njuszj@qq.com>
2020-02-29 23:12:00 +0800

    add skip list

--- textbook/GenSkipList/genSkipList.cpp
+++ textbook/GenSkipList/genSkipList.cpp
@@ -2,7 +2,7 @@
 
 template<class T>
 SkipList<T>::SkipList(){
-    for(int i=0;i<maxLevel;i++) root[i] = 0;    
+    for(int i=0;i<maxLevel;i++) root[i] = 0;    //初始化最小值为0
 }
 
 template<class T>
@@ -24,5 +24,67 @@ int SkipList<T>::chooseLevel(){
         if(r<powers[i])
             return i - 1;
     return i-1; 
+}
+
+template<class T>
+T* SkipList<T>::skipListSearch(const T& key){
+    if(isEmpty()) return 0;
+    nodePtr prev, curr;
+    int lvl;
+    for(lvl=maxLevel-1;lvl>=0 && !root[lvl];lvl--);  //从等级最高的节点依次向下搜索
+    prev = curr = root[lvl];
+    while(true){
+        if(key == curr->key) return &curr->key;
+        else if(key < curr->key){
+            if(lvl==0) return 0;
+            else if(curr==root[lvl]) curr = root[--lvl];
+            else curr = *(prev->next + --lvl);
+        }
+        else{
+            prev = curr;
+            if(*(curr->next + lvl)!=0) curr = *(curr->next + lvl);
+            else{
+                for(lvl--;lvl>=0 && *(curr->next + lvl)==0;lvl--);
+                if(lvl >= 0) curr = *(curr->next + lvl);
+                else return 0;
+            }
+        }
+    }
+}
+
+template<class T>
+void SkipList<T>::skipListInsert(const T& key){
+    nodePtr curr[maxLevel], prev[maxLevel], newNode;
+    int lvl, i;
+    curr[maxLevel-1] = root[maxLevel-1];
+    prev[maxLevel-1] = 0;
+    for(lvl = maxLevel-1; lvl >= 0; lvl--){
+        while(curr[lvl]&&curr[lvl]->key < key) {
+            prev[lvl] = curr[lvl];
+            curr[lvl] = *(curr[lvl]->next + lvl);
+        }
+        if(curr[lvl] && curr[lvl]->key == key)
+            return;
+        if(lvl>0)
+            if(prev[lvl]==0){
+                curr[lvl-1] = root[lvl-1];
+                prev[lvl-1] = 0;
+            }
+        else{
+            curr[lvl-1] = *(prev[lvl]->next + lvl-1);
+            prev[lvl-1] = prev[lvl];
+        }
+    }
+    lvl = chooseLevel(); //给新节点随机设置一个等级
+    newNode = new SkipListNode<T>;
+    newNode->next = new nodePtr[sizeof(nodePtr)*(lvl+1)];
+    newNode->key = key;
+    for(i=0;i<=lvl;i++){
+        *(newNode->next + i) = curr[i];
+        if(prev[i] == 0)
+            root[i] = newNode;
+        else *(prev[i]->next + i) = newNode;
+    }
+
 }
 
--- textbook/GenSkipList/genSkipList.h
+++ textbook/GenSkipList/genSkipList.h
@@ -1,14 +1,14 @@
-// 跳跃链表实现
+// 跳跃链表用于解决查找特定值困难的问题，是有序链表的扩展, 可以类比于平衡树
 // 1/2节点只有一个指针，1/4节点有两个指针, 1/8节点有三个指针，..., maxLevel = floor(lg n) + 1
 
-const int maxLevel = 4;
+const int maxLevel = 4;  //设置最大级别数
 
 template<class T>
 class SkipListNode{
 public:
     SkipListNode(){}
     T key;
-    SkipListNode **next;
+    SkipListNode **next;  //两个指针，一个指向下一个(>)，一个指向同一层(<)
 };
 
 template<class T>
@@ -16,8 +16,8 @@ class SkipList{
 public:
     SkipList();
     bool isEmpty() const;
-    void choosePowers();
-    int chooseLevel();
+    void choosePowers();  // 乘方
+    int chooseLevel();  // 随机选层
     T* skipListSearch(const T&);
     void skipListInsert(const T&);
 private:
